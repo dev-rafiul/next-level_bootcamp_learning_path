7e03a68c791327b0f7c75272e9dc3686b75913ee
COMRADE040 <rafiulislam040@gmail.com>
2026-05-02 13:12:42 +0600

    in object function define

--- src/function.ts
+++ src/function.ts
@@ -7,3 +7,19 @@ function add(num1: string, num2: number){
     return num1 + num2
 }
 console.log(add("rafiul", 12))
+
+
+const poorUser = {
+    name: "Mezba",
+    balance: 0,
+    addBalance(value: number): number{
+        const totalBalance = this.balance + value;
+        return totalBalance
+    }
+}
+poorUser.addBalance(100000)
+
+
+const arr: number[] = [1, 4, 6]
+
+const sqrArray = arr.map((element: number): number => element * element)
