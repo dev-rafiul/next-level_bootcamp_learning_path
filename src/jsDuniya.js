e16d81653ee01bf9a4042177c2bd2d3bea066636
COMRADE040 <rafiulislam040@gmail.com>
2026-05-03 01:46:23 +0600

    summary of basic typescript

--- src/jsDuniya.js
+++ src/jsDuniya.js
@@ -4,3 +4,20 @@ let username = "mezba123";
 username = 1234
 
 console.log(username)
+
+
+// Example starting here 
+
+
+// Typescript Basic topic
+
+
+// Destructuring
+// Function
+// Nonprimitive
+// Primitive
+// Nullable unknown never
+// Question mark 
+// Spread and rest operator
+// Type Alias
+// Union and intersection
